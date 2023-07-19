881193b729f2c4c1cc259602a6b40682410e265c
rtsoy <tsoiroman98@gmail.com>
2023-07-19 15:57:17 +0600

    Initialize Fiber

--- app/main.go
+++ app/main.go
@@ -1,1 +1,18 @@
-package app
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatalf("Error loading .env file: %s", err.Error())
+	}
+
+	app := fiber.New()
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
