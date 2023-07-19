4bb6f1006ada67fc83f31fb2f2ef570da21bea77
rtsoy <tsoiroman98@gmail.com>
2023-07-19 16:30:00 +0600

    Initialize Redis connection

--- app/redis/redis.go
+++ app/redis/redis.go
@@ -1,1 +1,19 @@
 package redis
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"os"
+)
+
+var ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.Client {
+	clientOptions := &redis.Options{
+		Addr:     os.Getenv("DB_ADDR"),
+		Password: os.Getenv("DB_PASS"),
+		DB:       dbNo,
+	}
+
+	return redis.NewClient(clientOptions)
+}
